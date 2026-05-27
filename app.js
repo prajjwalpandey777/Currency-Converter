f75ce4f5a6a46aa429709cd097de4c66a7767eb1
Prajjwal Pandey <prajjwalpandey171@gmail.com>
2026-05-27 23:28:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://open.er-api.com/v6/latest";
-
+ 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
@@ -7,32 +7,33 @@ const toCurr = document.querySelector(".to select");
 
 
 for (let select of dropdowns){
-    for (currCode in countryList) {
+    for (let currCode in countryList) {
         let newOption = document.createElement("option");
-        newOption.innerText = currCode;
-        newOption.value = currCode ;
+        const countryName = currencyNames[currCode] || "";
+        newOption.innerText = countryName ? `${currCode} - ${countryName}` : currCode;
+        newOption.value = currCode;
         if(select.name === "from" && currCode === "USD"){
-            newOption.selected = "selected"
+            newOption.selected = "selected";
         } else if(select.name === "to" && currCode === "INR"){
-            newOption.selected = "selected"
+            newOption.selected = "selected";
         }
         select.append(newOption);
     }
-
-    select.addEventListener("change",(evt) =>{
+ 
+    select.addEventListener("change", (evt) => {
         updateFlag(evt.target);
-    })
-};
-
-const updateFlag = (element) =>{
+    });
+}
+ 
+const updateFlag = (element) => {
     let currCode = element.value;
     let countryCode = countryList[currCode];
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
 }
-
-btn.addEventListener("click", async (evt) =>{
+ 
+btn.addEventListener("click", async (evt) => {
     evt.preventDefault();
     let amount = document.querySelector(".amount input");
     let amtVal = amount.value;
@@ -40,15 +41,11 @@ btn.addEventListener("click", async (evt) =>{
         amtVal = 1;
         amount.value = "1";
     }
-
+ 
     const URL = `${BASE_URL}/${fromCurr.value}`;
-
     let response = await fetch(URL);
-
     let data = await response.json();
-
     let rate = data.rates[toCurr.value];
-
     let convertedAmount = (amtVal * rate).toFixed(2);
     let msgEl = document.querySelector(".msg p");
     msgEl.innerText = `${amtVal} ${fromCurr.value} = ${convertedAmount} ${toCurr.value}`;
